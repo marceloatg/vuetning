874e4900537af168fbc08d724e298f34771fecec
Marcelo Gomes <marceloatg@outlook.com>
2019-02-19 20:12:25 -0300

    Implementação de estrutura base de aplicação

--- src/main.js
+++ src/main.js
@@ -1,9 +1,23 @@
 import Vue from 'vue'
 import App from './App.vue'
-import './vuetning'
+import SldsAlert from './components/Alert/Index'
+import SldsBrandBand from './components/BrandBand/Index'
+import SldsButton from './components/Button/Index'
+import SldsButtonIcon from './components/ButtonIcon/Index'
+import SldsGlobalHeader from './components/GlobalHeader/Index'
+import SldsGlobalNavigation from './components/GlobalNavigation/Index'
+import SldsSvg from './shared/Svg/index'
 
-Vue.config.productionTip = false
+Vue.component('slds-alert', SldsAlert);
+Vue.component('slds-brand-band', SldsBrandBand);
+Vue.component('slds-button', SldsButton);
+Vue.component('slds-button-icon', SldsButtonIcon);
+Vue.component('slds-global-header', SldsGlobalHeader);
+Vue.component('slds-global-navigation', SldsGlobalNavigation);
+Vue.component('slds-svg', SldsSvg);
+
+Vue.config.productionTip = false;
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+    render: h => h(App),
+}).$mount('#app');
