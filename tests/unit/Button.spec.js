9b4793e9bab17ca6a7a7fbb9f1be460403adbdae
Marcelo Gomes <marceloatg@outlook.com>
2019-02-21 00:57:47 -0300

    Criação de testes unitários para componente Button

---
+++ tests/unit/Button.spec.js
@@ -0,0 +1,58 @@
+import {mount, shallowMount} from '@vue/test-utils'
+import SldsButton from '../../src/components/Button/Index'
+
+describe('Index.vue', () => {
+    it('renders props.label when passed', () => {
+
+        // shallow mount the button setting a label
+        const label = 'test label';
+        const wrapper = shallowMount(SldsButton, {
+            propsData: {label}
+        });
+
+        // assert button text matches label
+        expect(wrapper.text()).toMatch(label);
+    });
+
+    it('emits click when clicked', () => {
+
+        // shallow mount the button
+        const wrapper = shallowMount(SldsButton);
+
+        // trigger click
+        wrapper.find('button').trigger('click');
+
+        // assert event has been emitted
+        expect(wrapper.emitted().click).toBeTruthy();
+    });
+
+    it('does no emit click when clicked in disabled state', () => {
+
+        // shallow mount the button setting a disabled state
+        const disabled = true;
+        const wrapper = shallowMount(SldsButton, {
+            propsData: {disabled}
+        });
+
+        // trigger click
+        wrapper.find('button').trigger('click');
+
+        // assert event has not been emitted
+        expect(wrapper.emitted().click).toBeFalsy();
+    });
+
+    it('does no emit click when clicked when spinner is active', () => {
+
+        // shallow mount the button setting a disabled state
+        const spinnerActive = true;
+        const wrapper = shallowMount(SldsButton, {
+            propsData: {spinnerActive}
+        });
+
+        // trigger click
+        wrapper.find('button').trigger('click');
+
+        // assert event has not been emitted
+        expect(wrapper.emitted().click).toBeFalsy();
+    });
+});
