424633b118b5c2a36f6ba09f68038e6af6f92ccd
Marcelo Gomes <marceloatg@outlook.com>
2019-02-22 23:03:03 -0300

    Criação de arquivo de configuração de Jest

---
+++ jest.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+    testResultsProcessor: 'jest-teamcity-reporter',
+    moduleFileExtensions: [
+        'js',
+        'jsx',
+        'json',
+        'vue'
+    ],
+    transform: {
+        '^.+\\.vue$': 'vue-jest',
+        '.+\\.(css|styl|less|sass|scss|svg|png|jpg|ttf|woff|woff2)$': 'jest-transform-stub',
+        '^.+\\.jsx?$': 'babel-jest'
+    },
+    moduleNameMapper: {
+        '^@/(.*)$': '<rootDir>/src/$1'
+    },
+    snapshotSerializers: [
+        'jest-serializer-vue'
+    ],
+    testMatch: [
+        "**/unit-tests.spec.(js|jsx|ts|tsx)|**/__tests__/*.(js|jsx|ts|tsx)"
+    ],
+    testURL: 'http://localhost/'
+};
