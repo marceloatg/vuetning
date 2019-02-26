bbb0937d7d48d553061357f321e0fcf39c99c378
Marcelo Gomes <marceloatg@outlook.com>
2019-02-26 10:20:55 -0300

    Alteração de regex de testes

--- jest.config.js
+++ jest.config.js
@@ -18,7 +18,7 @@ module.exports = {
         'jest-serializer-vue'
     ],
     testMatch: [
-        "**/unit-tests.spec.(js|jsx|ts|tsx)|**/__tests__/*.(js|jsx|ts|tsx)"
+        "**/unit.test.(js|jsx|ts|tsx)|**/__tests__/*.(js|jsx|ts|tsx)"
     ],
     testURL: 'http://localhost/',
     collectCoverage: true,
