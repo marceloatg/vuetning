9913e5a75b320aa1cf109b14cf2a56083c3fdf5d
Vinicius Damiati <viniciusdamiati@gmail.com>
2019-07-11 22:37:27 -0300

    fix

--- src/components/VirtualTable/Index.vue
+++ src/components/VirtualTable/Index.vue
@@ -93,7 +93,7 @@
 
                                     <!-- Boolean -->
                                     <span
-                                        v-if="column.type === 'boolean' && getFieldValue(column, item)"
+                                        v-else-if="column.type === 'boolean' && getFieldValue(column, item)"
                                         class="slds-truncate">
                                         <svg
                                             xmlns="http://www.w3.org/2000/svg"
@@ -101,7 +101,7 @@
                                             width="100%"
                                             style="width: .95rem;height: .95rem;"
                                             height="100%"
-                                            fill="#4bca81">
+                                            fill="#b0adab">
                                             <path d="M8.8 19.6L1.2 12c-.3-.3-.3-.8 0-1.1l1-1c.3-.3.8-.3 1 0L9 15.7c.1.2.5.2.6 0L20.9 4.4c.2-.3.7-.3 1 0l1 1c.3.3.3.7 0 1L9.8 19.6c-.2.3-.7.3-1 0z"/>
                                         </svg>
                                     </span>
