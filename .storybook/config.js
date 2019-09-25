002d615b7449bd33499095c5ef31246fe15a34ce
Marcelo Gomes <marceloatg@outlook.com>
2019-09-24 22:16:24 -0300

    Criação de button stories

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,4 +1,8 @@
 import { configure } from '@storybook/vue';
 
+// Importing styles
+import '../public/assets/styles/salesforce-lightning-design-system.min.css'
+import './style.css'
+
 // automatically import all files ending in *.stories.js
-configure(require.context('../stories', true, /\.stories\.js$/), module);
+configure(require.context('../src/components', true, /stories\.js$/), module);
---
+++ src/components/Button/stories.js
@@ -0,0 +1,93 @@
+import { storiesOf } from '@storybook/vue';
+import { withKnobs, text, boolean } from '@storybook/addon-knobs';
+import { action } from '@storybook/addon-actions';
+
+import SldsButton from './Index';
+
+const stories = storiesOf('Button', module);
+
+stories.addDecorator(withKnobs);
+
+stories.add('Basic', () => ({
+    components: { SldsButton },
+    props: {
+        disabled: {
+            default: boolean('Disabled', false)
+        },
+        label: {
+            default: text('Label', 'Basic Button')
+        },
+    },
+    template: '<slds-button :label="label" @click="action" :spinner="spinner" :disabled="disabled"/>',
+    methods: { action: action('clicked') },
+}));
+
+stories.add('Neutral', () => ({
+    components: { SldsButton },
+    props: {
+        disabled: {
+            default: boolean('Disabled', false)
+        },
+        label: {
+            default: text('Label', 'Neutral Button')
+        },
+        spinner: {
+            default: boolean('Spinner', false)
+        },
+    },
+    template: '<slds-button :label="label" neutral @click="action" :spinner="spinner" :disabled="disabled"/>',
+    methods: { action: action('clicked') },
+}));
+
+stories.add('Brand', () => ({
+    components: { SldsButton },
+    props: {
+        disabled: {
+            default: boolean('Disabled', false)
+        },
+        label: {
+            default: text('Label', 'Brand Button')
+        },
+        spinner: {
+            default: boolean('Spinner', false)
+        },
+    },
+    template: '<slds-button :label="label" brand @click="action" :spinner="spinner" :disabled="disabled"/>',
+    methods: { action: action('clicked') },
+}));
+
+/*
+export default {
+    title: 'Button',
+};
+
+export const basic = () => ({
+    components: { SldsButton },
+    template: '<slds-button label="Basic Button" @click="action"/>',
+    methods: { action: action('clicked') },
+});
+
+export const neutral = () => ({
+    components: { SldsButton },
+    template: '<slds-button label="Neutral Button" neutral @click="action"/>',
+    methods: { action: action('clicked') },
+});
+
+export const brand = () => ({
+    components: { SldsButton },
+    template: '<slds-button label="Brand Button" brand @click="action"/>',
+    methods: { action: action('clicked') },
+});
+
+export const success = () => ({
+    components: { SldsButton },
+    template: '<slds-button label="Success Button" success @click="action"/>',
+    methods: { action: action('clicked') },
+});
+
+export const destructive = () => ({
+    components: { SldsButton },
+    template: '<slds-button label="Destructive Button" destructive @click="action"/>',
+    methods: { action: action('clicked') },
+});
+*/
