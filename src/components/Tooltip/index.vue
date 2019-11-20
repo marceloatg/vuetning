088404bbc3b655590d7e72bd24e567cd476f9108
Marcelo Gomes <marceloatg@outlook.com>
2019-11-20 18:07:22 -0300

    Implementação de lógica de visibilidade em tooltip

--- src/components/Tooltip/index.vue
+++ src/components/Tooltip/index.vue
@@ -1,42 +1,35 @@
 <template>
-    <div class="slds-popover slds-popover_tooltip" :class="nubbinPlacements" role="tooltip">
-        <div class="slds-popover__body">
-            <slot/>
-        </div>
+    <div class="slds-tooltip__container" :style="containerStyle">
+
+        <slot name="trigger"/>
+
+        <transition name="fade">
+            <div
+                v-if="tooltipVisible"
+                ref="tooltip"
+                class="slds-popover slds-popover_tooltip"
+                :class="nubbinPlacements"
+                :style="tooltipStyle"
+                role="tooltip">
+                <div class="slds-popover__body">
+                    <slot/>
+                </div>
+            </div>
+        </transition>
+
     </div>
 </template>
 
 <script>
     export default {
         props: {
-            topRight: {
-                type: Boolean,
-            },
-            top: {
-                type: Boolean,
-            },
-            topLeft: {
-                type: Boolean,
-            },
-            bottomRight: {
-                type: Boolean,
-            },
             bottom: {
                 type: Boolean,
             },
             bottomLeft: {
                 type: Boolean,
             },
-            rightTop: {
-                type: Boolean,
-            },
-            right: {
-                type: Boolean,
-            },
-            leftTop: {
-                type: Boolean,
-            },
-            rightBottom: {
+            bottomRight: {
                 type: Boolean,
             },
             left: {
@@ -45,8 +38,46 @@
             leftBottom: {
                 type: Boolean,
             },
+            leftTop: {
+                type: Boolean,
+            },
+            right: {
+                type: Boolean,
+            },
+            rightBottom: {
+                type: Boolean,
+            },
+            rightTop: {
+                type: Boolean,
+            },
+            top: {
+                type: Boolean,
+            },
+            topLeft: {
+                type: Boolean,
+            },
+            topRight: {
+                type: Boolean,
+            },
+        },
+        data() {
+            return {
+                containerStyle: {
+                    position: 'relative',
+                },
+                tooltipStyle: {
+                    position: 'absolute',
+                    top: '-104px',
+                    left: '-17px',
+                },
+                tooltipVisible: false,
+                tooltipTrigger: null,
+            }
         },
         computed: {
+            clickable() {
+                return this.triggerElement.classList.contains('slds-tooltip-trigger_click');
+            },
             nubbinPlacements() {
                 if (this.left) return 'slds-nubbin_left';
                 else if (this.leftTop) return 'slds-nubbin_left-top';
@@ -62,6 +93,69 @@
                 else if (this.bottomRight) return 'slds-nubbin_bottom-right';
                 else return null;
             },
+            triggerElement() {
+                return this.$el.querySelector('.slds-tooltip-trigger_hover, .slds-tooltip-trigger_click');
+            },
+        },
+        mounted() {
+            this.setupTriggerListener();
+        },
+        beforeDestroy() {
+            this.removeEventListeners();
+        },
+        methods: {
+            clickAwayDetector(event) {
+                const tooltipElement = this.$refs.tooltip;
+                let targetElement = event.target;
+
+                do {
+                    if ((targetElement === tooltipElement) || (targetElement === this.triggerElement)) return;
+                    targetElement = targetElement.parentNode;
+                } while (targetElement);
+
+                this.hideTooltip();
+            },
+            hideTooltip() {
+                this.tooltipVisible = false;
+                if (this.clickable) document.removeEventListener("click", this.clickAwayDetector);
+            },
+            removeEventListeners() {
+                if (this.triggerElement == null) return;
+
+                if (this.clickable) {
+                    this.triggerElement.removeEventListener("click", this.showTooltip);
+                    document.addEventListener("click", this.clickAwayDetector);
+                }
+                else {
+                    this.triggerElement.removeEventListener("mouseenter", this.showTooltip);
+                    this.triggerElement.removeEventListener("mouseleave", this.hideTooltip);
+                }
+            },
+            setupTriggerListener() {
+                if (this.triggerElement == null) return;
+
+                if (this.clickable) {
+                    this.triggerElement.addEventListener("click", this.showTooltip);
+                }
+                else {
+                    this.triggerElement.addEventListener("mouseenter", this.showTooltip);
+                    this.triggerElement.addEventListener("mouseleave", this.hideTooltip);
+                }
+            },
+            showTooltip() {
+                this.tooltipVisible = true;
+                if (this.clickable) document.addEventListener("click", this.clickAwayDetector);
+            },
         },
     }
 </script>
+
+<style scoped lang="scss">
+    .fade-enter-active, .fade-leave-active {
+        transition: opacity 300ms;
+    }
+
+    .fade-enter, .fade-leave-to {
+        opacity: 0;
+    }
+</style>
--- src/views/Tooltip.vue
+++ src/views/Tooltip.vue
@@ -34,100 +34,174 @@
             </h2>
 
             <div style="padding-left:2rem;padding-top:6.75rem;position:relative">
-
                 <a href="javascript:void(0)" aria-describedby="help">
                     <span class="slds-icon_container slds-icon-utility-info">
                         <svg class="slds-icon slds-icon slds-icon_xx-small slds-icon-text-default" aria-hidden="true">
                             <use xlink:href="/assets/icons/utility-sprite/svg/symbols.svg#info"/>
                         </svg>
+                        <span class="slds-assistive-text">Click here to learn more</span>
                     </span>
                 </a>
-
-                <slds-tooltip style="position:absolute;top:-4px;left:15px">
-                    Sit nulla est ex deserunt exercitation anim occaecat. Nostrud ullamco deserunt
-                    aute id consequat veniam incididunt duis in sint irure nisi.
-                    <div class="slds-m-top_x-small" aria-hidden="true">Click
-                        <span class="slds-icon_container slds-icon-utility-info" title="Help">
-                            <svg class="slds-icon slds-icon slds-icon_xx-small" aria-hidden="true">
-                                <use xlink:href="/assets/icons/utility-sprite/svg/symbols.svg#info"/>
-                            </svg>
-                            <span class="slds-assistive-text">Help</span>
-                        </span> to learn more.
+                <div
+                    id="help"
+                    class="slds-popover slds-popover_tooltip slds-nubbin_bottom-left"
+                    role="tooltip"
+                    style="position:absolute;top:-4px;left:15px">
+                    <div class="slds-popover__body">Sit nulla est ex deserunt exercitation anim occaecat. Nostrud
+                        ullamco deserunt aute id consequat veniam incididunt duis in sint irure nisi.
+                        <div class="slds-m-top_x-small" aria-hidden="true">Click
+                            <span class="slds-icon_container slds-icon-utility-info" title="Help">
+                                <svg class="slds-icon slds-icon slds-icon_xx-small" aria-hidden="true">
+                                    <use xlink:href="/assets/icons/utility-sprite/svg/symbols.svg#info"/>
+                                </svg>
+                                <span class="slds-assistive-text">Help</span>
+                            </span> to learn more.
+                        </div>
                     </div>
-                </slds-tooltip>
-
+                </div>
             </div>
 
-            <h2 class="slds-text-heading_large slds-m-top_large slds-m-bottom_small">
-                Placement
-            </h2>
+            <br>
+            <br>
+            <br>
+            <br>
+            <br>
+            <br>
+            <br>
 
-            <slds-tooltip left class="slds-m-around_medium">
+            <slds-tooltip bottom-left>
+
+                <template #trigger>
+                    <a class="slds-tooltip-trigger_hover">
+                        <span class="slds-icon_container slds-icon-utility-info">
+                            <svg class="slds-icon slds-icon slds-icon_xx-small slds-icon-text-default">
+                                <use xlink:href="/assets/icons/utility-sprite/svg/symbols.svg#info"/>
+                            </svg>
+                        </span>
+                    </a>
+                </template>
+
                 Sit nulla est ex deserunt exercitation anim occaecat. Nostrud ullamco deserunt
                 aute id consequat veniam incididunt duis in sint irure nisi.
+
+                <div class="slds-m-top_x-small" aria-hidden="true">Click
+                    <span class="slds-icon_container slds-icon-utility-info" title="Help">
+                        <svg class="slds-icon slds-icon slds-icon_xx-small" aria-hidden="true">
+                            <use xlink:href="/assets/icons/utility-sprite/svg/symbols.svg#info"/>
+                        </svg>
+                        <span class="slds-assistive-text">Help</span>
+                    </span> to learn more.
+                </div>
+
             </slds-tooltip>
 
-            <slds-tooltip left-top class="slds-m-around_medium">
-                Sit nulla est ex deserunt exercitation anim occaecat. Nostrud ullamco deserunt
-                aute id consequat veniam incididunt duis in sint irure nisi.
-            </slds-tooltip>
+            <br>
+            <br>
+            <br>
+            <br>
+            <br>
+            <br>
+            <br>
 
-            <slds-tooltip left-bottom class="slds-m-around_medium">
+            <slds-tooltip bottom-left>
+
+                <template #trigger>
+                    <a class="slds-tooltip-trigger_click">
+                        <span class="slds-icon_container slds-icon-utility-info">
+                            <svg class="slds-icon slds-icon slds-icon_xx-small slds-icon-text-default">
+                                <use xlink:href="/assets/icons/utility-sprite/svg/symbols.svg#touch_action"/>
+                            </svg>
+                        </span>
+                    </a>
+                </template>
+
                 Sit nulla est ex deserunt exercitation anim occaecat. Nostrud ullamco deserunt
                 aute id consequat veniam incididunt duis in sint irure nisi.
+
+                <div class="slds-m-top_x-small" aria-hidden="true">Click
+                    <span class="slds-icon_container slds-icon-utility-info" title="Help">
+                        <svg class="slds-icon slds-icon slds-icon_xx-small" aria-hidden="true">
+                            <use xlink:href="/assets/icons/utility-sprite/svg/symbols.svg#info"/>
+                        </svg>
+                        <span class="slds-assistive-text">Help</span>
+                    </span> to learn more.
+                </div>
+
             </slds-tooltip>
 
             <br>
 
-            <slds-tooltip top class="slds-m-around_medium">
-                Sit nulla est ex deserunt exercitation anim occaecat. Nostrud ullamco deserunt
-                aute id consequat veniam incididunt duis in sint irure nisi.
-            </slds-tooltip>
+            <!--
+                        <h2 class="slds-text-heading_large slds-m-top_large slds-m-bottom_small">
+                            Placement
+                        </h2>
 
-            <slds-tooltip top-left class="slds-m-around_medium">
-                Sit nulla est ex deserunt exercitation anim occaecat. Nostrud ullamco deserunt
-                aute id consequat veniam incididunt duis in sint irure nisi.
-            </slds-tooltip>
+                        <slds-tooltip left class="slds-m-around_medium">
+                            Sit nulla est ex deserunt exercitation anim occaecat. Nostrud ullamco deserunt
+                            aute id consequat veniam incididunt duis in sint irure nisi.
+                        </slds-tooltip>
 
-            <slds-tooltip top-right class="slds-m-around_medium">
-                Sit nulla est ex deserunt exercitation anim occaecat. Nostrud ullamco deserunt
-                aute id consequat veniam incididunt duis in sint irure nisi.
-            </slds-tooltip>
+                        <slds-tooltip left-top class="slds-m-around_medium">
+                            Sit nulla est ex deserunt exercitation anim occaecat. Nostrud ullamco deserunt
+                            aute id consequat veniam incididunt duis in sint irure nisi.
+                        </slds-tooltip>
 
-            <br>
+                        <slds-tooltip left-bottom class="slds-m-around_medium">
+                            Sit nulla est ex deserunt exercitation anim occaecat. Nostrud ullamco deserunt
+                            aute id consequat veniam incididunt duis in sint irure nisi.
+                        </slds-tooltip>
 
-            <slds-tooltip right class="slds-m-around_medium">
-                Sit nulla est ex deserunt exercitation anim occaecat. Nostrud ullamco deserunt
-                aute id consequat veniam incididunt duis in sint irure nisi.
-            </slds-tooltip>
+                        <br>
 
-            <slds-tooltip right-top class="slds-m-around_medium">
-                Sit nulla est ex deserunt exercitation anim occaecat. Nostrud ullamco deserunt
-                aute id consequat veniam incididunt duis in sint irure nisi.
-            </slds-tooltip>
+                        <slds-tooltip top class="slds-m-around_medium">
+                            Sit nulla est ex deserunt exercitation anim occaecat. Nostrud ullamco deserunt
+                            aute id consequat veniam incididunt duis in sint irure nisi.
+                        </slds-tooltip>
 
-            <slds-tooltip right-bottom class="slds-m-around_medium">
-                Sit nulla est ex deserunt exercitation anim occaecat. Nostrud ullamco deserunt
-                aute id consequat veniam incididunt duis in sint irure nisi.
-            </slds-tooltip>
+                        <slds-tooltip top-left class="slds-m-around_medium">
+                            Sit nulla est ex deserunt exercitation anim occaecat. Nostrud ullamco deserunt
+                            aute id consequat veniam incididunt duis in sint irure nisi.
+                        </slds-tooltip>
 
-            <br>
+                        <slds-tooltip top-right class="slds-m-around_medium">
+                            Sit nulla est ex deserunt exercitation anim occaecat. Nostrud ullamco deserunt
+                            aute id consequat veniam incididunt duis in sint irure nisi.
+                        </slds-tooltip>
 
-            <slds-tooltip bottom class="slds-m-around_medium">
-                Sit nulla est ex deserunt exercitation anim occaecat. Nostrud ullamco deserunt
-                aute id consequat veniam incididunt duis in sint irure nisi.
-            </slds-tooltip>
+                        <br>
 
-            <slds-tooltip bottom-left class="slds-m-around_medium">
-                Sit nulla est ex deserunt exercitation anim occaecat. Nostrud ullamco deserunt
-                aute id consequat veniam incididunt duis in sint irure nisi.
-            </slds-tooltip>
+                        <slds-tooltip right class="slds-m-around_medium">
+                            Sit nulla est ex deserunt exercitation anim occaecat. Nostrud ullamco deserunt
+                            aute id consequat veniam incididunt duis in sint irure nisi.
+                        </slds-tooltip>
 
-            <slds-tooltip bottom-right class="slds-m-around_medium">
-                Sit nulla est ex deserunt exercitation anim occaecat. Nostrud ullamco deserunt
-                aute id consequat veniam incididunt duis in sint irure nisi.
-            </slds-tooltip>
+                        <slds-tooltip right-top class="slds-m-around_medium">
+                            Sit nulla est ex deserunt exercitation anim occaecat. Nostrud ullamco deserunt
+                            aute id consequat veniam incididunt duis in sint irure nisi.
+                        </slds-tooltip>
 
+                        <slds-tooltip right-bottom class="slds-m-around_medium">
+                            Sit nulla est ex deserunt exercitation anim occaecat. Nostrud ullamco deserunt
+                            aute id consequat veniam incididunt duis in sint irure nisi.
+                        </slds-tooltip>
+
+                        <br>
+
+                        <slds-tooltip bottom class="slds-m-around_medium">
+                            Sit nulla est ex deserunt exercitation anim occaecat. Nostrud ullamco deserunt
+                            aute id consequat veniam incididunt duis in sint irure nisi.
+                        </slds-tooltip>
+
+                        <slds-tooltip bottom-left class="slds-m-around_medium">
+                            Sit nulla est ex deserunt exercitation anim occaecat. Nostrud ullamco deserunt
+                            aute id consequat veniam incididunt duis in sint irure nisi.
+                        </slds-tooltip>
+
+                        <slds-tooltip bottom-right class="slds-m-around_medium">
+                            Sit nulla est ex deserunt exercitation anim occaecat. Nostrud ullamco deserunt
+                            aute id consequat veniam incididunt duis in sint irure nisi.
+                        </slds-tooltip>
+            -->
         </div>
 
     </article>
