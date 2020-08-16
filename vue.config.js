351954a5d90023353c484db8cff59cd10eeef4bf
Allan Oricil <allanoricilcos@outlook.com>
2020-08-16 18:04:05 -0300

    Add feature/inline-svg-icons to project

--- vue.config.js
+++ vue.config.js
@@ -1,12 +1,5 @@
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-
 module.exports = {
   css: {
     extract: false
-  },
-  configureWebpack: {
-    plugins: [
-      new CleanWebpackPlugin()
-    ]
   }
 }
