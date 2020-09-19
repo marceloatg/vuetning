360ec2e2c2ab76a957cc04a9beb7169f6cb08fff
Marcelo Gomes <marceloatg@outlook.com>
2020-09-19 13:20:32 -0300

    Prompt keyup events updated

--- src/components/slds-prompt/index.vue
+++ src/components/slds-prompt/index.vue
@@ -1,5 +1,5 @@
 <template>
-    <div>
+    <div @keyup.esc="onClose" @keyup.enter="onSubmit">
 
         <!-- Modal -->
         <section tabindex="0" class="slds-modal slds-fade-in-open slds-modal_prompt">
@@ -69,22 +69,9 @@ export default {
         },
     },
 
-    created() {
-        document.body.addEventListener("keyup", this.onKeyUp);
-    },
-
-    beforeDestroy() {
-        document.body.removeEventListener("keyup", this.onKeyUp);
-    },
-
     methods: {
         onClose() {
             this.$emit('close');
-        },
-
-        onKeyUp(event) {
-            if (event.key === 'Escape') this.onClose();
-            else if (event.key === 'Enter') this.onSubmit();
         },
 
         onSubmit() {
