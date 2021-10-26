605a3443cf57afea6aacfa39ef6754051b4d253c
Marcelo Gomes <marceloatg@outlook.com>
2021-10-26 17:44:28 -0300

    Page header updated

--- src/views/components/PageHeader.vue
+++ src/views/components/PageHeader.vue
@@ -13,41 +13,83 @@
         </slds-page-header>
 
         <slds-page-header
-            base
             icon="standard:opportunity"
             title="Rohde Corp - 80,000 Widgets"
-            details="Mark Jaeckal • Unlimited Customer • 11/13/15"
+            meta="Mark Jaeckal • Unlimited Customer • 11/13/15"
             class="slds-m-top_medium"
         />
 
         <slds-page-header
+            object-home
             icon="standard:opportunity"
+            name="Opportunities"
             title="Recently Viewed"
-            details="Opportunities"
+            meta="10 items • Updated 13 minutes ago"
             class="slds-m-top_medium"
             has-dropdown
         >
-            <template #dropdown>
-                <p class="slds-p-around_large">
-                    Dropdown
-                </p>
+
+            <template #actions>
+                <slds-button neutral label="New"/>
             </template>
+
+            <template #controls>
+                <slds-button-group>
+                    <slds-button-icon bordered-filled icon="utility:copy"/>
+                    <slds-button-icon bordered-filled icon="utility:edit"/>
+                    <slds-button-icon bordered-filled icon="utility:delete"/>
+                </slds-button-group>
+            </template>
+
         </slds-page-header>
 
         <slds-page-header
+            record-home
             icon="standard:opportunity"
-            title="Recently Viewed"
+            name="Opportunities"
+            title="Acme - 1,200 Widgets"
             class="slds-m-top_medium"
         >
-            <template #breadcrumb>
-                <slds-breadcrumb/>
+
+            <template #actions>
+                <slds-button neutral label="New"/>
             </template>
+
+            <template #details>
+                <slds-page-header-detail title="Field 1" value="Description that demonstrates truncation with a long text field."/>
+                <slds-page-header-detail title="Field " value="Hyperlink" is-link/>
+            </template>
+
+        </slds-page-header>
+
+        <slds-page-header
+            related-list
+            name="Opportunities"
+            title="Recently Viewed"
+            meta="10 items • Updated 13 minutes ago"
+            class="slds-m-top_medium"
+            has-dropdown
+        >
+
+            <template #actions>
+                <slds-button neutral label="New"/>
+            </template>
+
+            <template #controls>
+                <slds-button-group>
+                    <slds-button-icon bordered-filled icon="utility:copy"/>
+                    <slds-button-icon bordered-filled icon="utility:edit"/>
+                    <slds-button-icon bordered-filled icon="utility:delete"/>
+                </slds-button-group>
+            </template>
+
         </slds-page-header>
 
     </main>
 </template>
 
 <script>
+
 export default {
     name: 'PageHeader',
 }
