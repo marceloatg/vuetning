a3fe45004b5b25ca19c1d0881f884f178bb7e9bd
Marcelo Gomes <marceloatg@outlook.com>
2022-04-07 15:01:49 -0300

    slds-multi-picklist implemented

---
+++ src/components/slds-multi-picklist/slds-multi-picklist-option.vue
@@ -0,0 +1,42 @@
+<template>
+    <slds-dropdown-option
+        :label="label"
+        :meta="meta"
+        :disabled="disabled"
+        is-selectable
+        :is-selected="isSelected"
+        :has-focus="hasFocus"
+        @click="onClick"
+        @mouseover="onMouseOver"
+    />
+</template>
+
+<script>
+import SldsDropdownOption from '@/components/slds-options/slds-dropdown-option'
+
+export default {
+    name: 'SldsMultiPicklistOption',
+
+    components: {
+        SldsDropdownOption
+    },
+
+    props: {
+        disabled: Boolean,
+        hasFocus: Boolean,
+        isSelected: Boolean,
+        label: {type: String, required: true},
+        meta: String
+    },
+
+    methods: {
+        onClick() {
+            this.$emit('click')
+        },
+
+        onMouseOver() {
+            this.$emit('mouseover')
+        },
+    }
+}
+</script>
