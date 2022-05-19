04dc3aaf73af87eb20dbf7de0e28d372e4b7c065
Gustavo Gomes <gustavo.gomes@bluebear.app>
2022-05-19 15:51:38 -0300

    slds-tile initial implementation

---
+++ src/components/slds-tile/slds-tile-article.vue
@@ -0,0 +1,37 @@
+<template>
+    <div>
+
+        <p>{{ author }}</p>
+
+        <ul class="slds-list_horizontal slds-has-dividers_right">
+            <li
+                v-for="item in items"
+                :key="item"
+                class="slds-item"
+            >
+                {{ item }}
+            </li>
+        </ul>
+
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'SldsTileArticle',
+    
+    props: {
+        /**
+         * @description Article author name.
+         * @type {string}
+         */
+        author: String,
+
+        /**
+         * @description List of items present in the article.
+         * @type {string}
+         */
+        items: Array,
+    }
+}
+</script>
---
+++ src/components/slds-tile/slds-tile-item.vue
@@ -0,0 +1,33 @@
+<template>
+    <dl class="slds-list_horizontal slds-wrap">
+
+        <dt class="slds-item_label slds-text-color_weak slds-truncate">
+            {{ label }}
+        </dt>
+
+        <dd class="slds-item_detail slds-truncate">
+            {{ detail }}
+        </dd>
+
+    </dl>
+</template>
+
+<script>
+export default {
+    name: 'SldsTileItem',
+
+    props: {
+        /**
+         * @description The text detail that displays in the tile item.
+         * @type {string}
+         */
+        detail: String,
+
+        /**
+         * @description The text label that displays in the tile item.
+         * @type {string}
+         */
+        label: String,
+    }
+}
+</script>
